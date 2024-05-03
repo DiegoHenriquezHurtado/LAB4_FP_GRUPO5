6af426c53827322b0afbc9c5c78f7f857639ac8b
Diego Henriquez <a20182044@pucp.edu.pe>
2024-05-03 10:39:23 -0500

    primer commit

---
+++ src/main/java/com/example/lab4_fp_grupo5/Lab4FpGrupo5Application.java
@@ -0,0 +1,13 @@
+package com.example.lab4_fp_grupo5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4FpGrupo5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab4FpGrupo5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab4_fp_grupo5/Lab4FpGrupo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab4_fp_grupo5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4FpGrupo5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
